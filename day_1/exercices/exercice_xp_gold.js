07618df1d212034a8e7cf4289f93b5b33224cc9a
walid-habibi-2004 <habibiwalid553@gmail.com>
2026-02-07 15:04:14 +0100

    Update exercice_xp_gold.js

--- day_1/exercices/exercice_xp_gold.js
+++ day_1/exercices/exercice_xp_gold.js
@@ -1,53 +1,85 @@
-// ========= exercice 1
+// ===== Exercise 1
+//1 - flat(4)
 
-function printFullName({ first, last }) {
-  return `Your full name is ${first} ${last}`;
+// The flat function adds _ x times.
+// x = 4 → adds "____"
+// result = "____"
+
+//2 - mountain(4)
+// Adds / → result = "____/"
+// Adds ' x times → 4 ' → result = "____/''''"
+// Adds \ → result = "____/''''\"
+
+//3 - flat(4) again
+// Adds 4 underscores → result = "____/''''\____
+
+let landscape = () => {
+    let result = "";
+
+    let flat = (x) => {
+        for (let count = 0; count < x; count++) {
+            result += "_";
+        }
+    }
+
+    let mountain = (x) => {
+        result += "/";
+        for (let counter = 0; counter < x; counter++) {
+            result += "'";
+        }
+        result += "\\";
+    }
+
+    flat(4);
+    mountain(4);
+    flat(4);
+
+    return result;
 }
 
-console.log(printFullName({ first: 'Elie', last: 'Schoppik' }));
+landscape();
 
-function printFullName(objUser) {
-    const { first, last } = objUser;
-    return `Your full name is ${first} ${last}`;
-}
+// ===== Exercise 2
 
-console.log(printFullName({ first: 'Elie', last: 'Schoppik' }));
+const addTo = x => y => x + y;
+const addToTen = addTo(10);
+addToTen(3);
 
-// ======== exercice 2
+// 1 - addTo = x => y => x + y
+// this function take x and return new function the new take y ana return x + y
+// 2 - addToTen = addTo(10)
+// x = 10 so addToTen becomes a function take y and return y + 10
+// 3 - addToTen(3)
+// y = 3 , 3 + 10 = 13
+// So the result is 13
 
-function keysAndValues(obj) {
-    const keys = Object.keys(obj).sort();
-    
-    const values = keys.map(key => obj[key]);
-    
-    return [keys, values];
-}
+// ===== Exercise 3
+const curriedSum = (a) => (b) => a + b
+curriedSum(30)(1)
 
-console.log(keysAndValues({ a: 1, b: 2, c: 3 }));
-console.log(keysAndValues({ a: "Apple", b: "Microsoft", c: "Google" }));
-console.log(keysAndValues({ key1: true, key2: false, key3: undefined }));
+// this function take a and return new function the new take b and return a + b
+// curriedSum(30) a = 30 ---> 30 + b
+// curriedSum(30)(1) b = 1 ---> 30  + 1
+// So the result is 31
 
-// ========= exercice 3
+// ===== Exercise 4
 
-class Counter {
-  constructor() {
-    this.count = 0;
-  }
+const curriedSum = (a) => (b) => a + b
+const add5 = curriedSum(5)
+add5(12)
 
-  increment() {
-    this.count++;
-  }
-}
+// curriedSum(5) return new fonction his take 5 + b
+// add5(12) take 12 → return 5 + 12 = 17
+// This is how you create a mini-function that remembers a fixed number (5) and adds any number you give it.
 
-const counterOne = new Counter(); // count = 0
-counterOne.increment();           // count = 1
-counterOne.increment();           // count = 2
 
-const counterTwo = counterOne;    // counterTwo pointe sur le même objet que counterOne
-counterTwo.increment();           // count = 3
+// ===== Exercise 5
 
-console.log(counterOne.count);    // 3
+const compose = (f, g) => (a) => f(g(a));
+const add1 = (num) => num + 1;
+const add5 = (num) => num + 5;
+compose(add1, add5)(10)
 
-// counterOne and counterTwo refer to the same object.
-// Every call to increment() increases count by 1 on that same object.
-// Therefore, the final value of counterOne.count is 3.
+// compose(add1, add5) create a new fonction his make : add1(add5(a))
+// right now a = 10 → add5(10) = 15 → add1(15) = 16
+// So final output = 16
