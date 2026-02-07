840ea4ab096e0065c2c13bd66557c009726c3d52
walid-habibi-2004 <habibiwalid553@gmail.com>
2026-02-07 14:49:41 +0100

    Update exercices_xp_ninja.js

---
+++ day_3/exercices/exercices_xp_ninja.js
@@ -0,0 +1,16 @@
+class Bird {
+  constructor() {
+    console.log("I'm a bird. 🦢");
+  }
+}
+
+class Flamingo extends Bird {
+  constructor() {
+    console.log("I'm pink. 🌸");
+    super();
+  }
+}
+
+const pet = new Flamingo();
+
+//  ReferenceError: Must call super constructor in derived class before accessing 'this' or returning from derived constructor
