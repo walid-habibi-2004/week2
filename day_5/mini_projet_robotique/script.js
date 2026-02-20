e3e26ddc444df8ed1cf88246ae235538888b27d5
walid-habibi-2004 <habibiwalid553@gmail.com>
2026-02-20 11:13:21 +0000

    Update script.js

--- day_5/mini_projet_robotique/script.js
+++ day_5/mini_projet_robotique/script.js
@@ -1,1 +1,109 @@
 
+const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz',
+        image: 'https://robohash.org/1?200x200'
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@melissa.tv',
+        image: 'https://robohash.org/2?200x200'
+    },
+    {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'Nathan@yesenia.net',
+        image: 'https://robohash.org/3?200x200'
+    },
+    {
+        id: 4,
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        email: 'Julianne.OConner@kory.org',
+        image: 'https://robohash.org/4?200x200'
+    },
+    {
+        id: 5,
+        name: 'Chelsey Dietrich',
+        username: 'Kamren',
+        email: 'Lucio_Hettinger@annie.ca',
+        image: 'https://robohash.org/5?200x200'
+    },
+    {
+        id: 6,
+        name: 'Mrs. Dennis Schulist',
+        username: 'Leopoldo_Corkery',
+        email: 'Karley_Dach@jasper.info',
+        image: 'https://robohash.org/6?200x200'
+    },
+    {
+        id: 7,
+        name: 'Kurtis Weissnat',
+        username: 'Elwyn.Skiles',
+        email: 'Telly.Hoeger@billy.biz',
+        image: 'https://robohash.org/7?200x200'
+    },
+    {
+        id: 8,
+        name: 'Nicholas Runolfsdottir V',
+        username: 'Maxime_Nienow',
+        email: 'Sherwood@rosamond.me',
+        image: 'https://robohash.org/8?200x200'
+    },
+    {
+        id: 9,
+        name: 'Glenna Reichert',
+        username: 'Delphine',
+        email: 'Chaim_McDermott@dana.io',
+        image: 'https://robohash.org/9?200x200'
+    },
+    {
+        id: 10,
+        name: 'Clementina DuBuque',
+        username: 'Moriah.Stanton',
+        email: 'Rey.Padberg@karina.biz',
+        image: 'https://robohash.org/10?200x200'
+    }
+];
+let robots2 = robots
+const main = document.querySelector("main")
+const display = (robots) => {
+
+    for (const robot of robots) {
+        let div = document.createElement("div")
+        div.setAttribute("id", "cards")
+        main.appendChild(div)
+        let h2 = document.createElement("h2")
+        h2.textContent = robot.name
+        let p = document.createElement("p")
+        p.textContent = robot.email
+        let image = document.createElement("img")
+        image.setAttribute("src", robot.image)
+
+        div.appendChild(image)
+        div.appendChild(h2)
+        div.appendChild(p)
+    }
+}
+
+display(robots)
+
+const input = document.getElementById("search")
+input.addEventListener("input", (e) => {
+
+    let search = robots.filter(robot => robot.name.toLowerCase().startsWith(e.target.value.toLowerCase()))
+    if (search) {
+        main.innerHTML = ""
+        display(search)
+    } else {
+        let p = document.createElement("p")
+        p.textContent = "Item not found"
+    }
+
+})
