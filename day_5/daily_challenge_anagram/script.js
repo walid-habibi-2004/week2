e83badd22c9c18f28459b035b150fc6fc86b8ef0
walid-habibi-2004 <habibiwalid553@gmail.com>
2026-02-21 13:22:20 +0000

    Implement isAnagram function to check anagrams

--- day_5/daily_challenge_anagram/script.js
+++ day_5/daily_challenge_anagram/script.js
@@ -1,1 +1,17 @@
+function isAnagram(str1, str2) {
+  const format = (str) =>
+    str
+      .toLowerCase()
+      .replace(/\s+/g, '')
+      .split('')
+      .sort()
+      .join('');
 
+  return format(str1) === format(str2);
+}
+
+
+console.log(isAnagram("Astronomer", "Moon starer"));
+console.log(isAnagram("School master", "The classroom"));
+console.log(isAnagram("The Morse Code", "Here come dots"));
+console.log(isAnagram("Hello", "World"));
